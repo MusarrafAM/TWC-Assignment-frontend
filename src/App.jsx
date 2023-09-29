42f14303cc98049739941e4a8a8828254845ef82
Musarraf <muhammedmusarraf@gmail.com>
2023-09-30 01:14:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="h-screen">
+      <div className="z-0 absolute bg-[#083F46] w-[1502px] h-[1502px] rounded-full left-[-716px] top-[-367px]"></div>
+      <section className="relative w-[477px] h-[487px] top-[140px] left-[122px] ">
+        <p className="text-white text-5xl font-bold pb-4">Hi there,</p>
+        <p className="text-white text-3xl">
+          Welcome to our <br />
+          contacts portal
+        </p>
+        
+      </section>
+    </main>
+  );
+}
