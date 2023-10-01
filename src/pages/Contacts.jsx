d58e2643c1300ad41443e0c246697cfa780517e5
Musarraf <muhammedmusarraf@gmail.com>
2023-10-01 10:39:27 +0530

    finished contacts page

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
+import { data } from "../constants";
+
+
 const Contacts = () => {
   const Navigate = useNavigate();
 
@@ -25,10 +28,11 @@ const Contacts = () => {
         </p>
       </div>
 
-      <div className="absolute text-white w-[994px] h-[73px] top-[278px] left-[204px]">
+      <div className="absolute text-white w-[994px] h-[73px] top-[230px] left-[204px]">
         <p className=" inline-flex text-4xl font-bold mb-[60px] pt-2">
           Contacts
         </p>
+
         <button
           onClick={() => got_To_Add_Contacts()}
           className=" ml-[520px] rounded-full border focus:shadow-outline w-[255px] h-[48px] hover:bg-white hover:text-[#083F46]"
@@ -36,6 +40,34 @@ const Contacts = () => {
           Add new contact
         </button>
       </div>
+
+      <section className="contact-details overflow-y-auto absolute w-[955px] h-[316px] bg-white mt-[220px] ml-[200px] rounded-[30px]">
+        <div className="table-heading flex space-x-[100px] pl-[144px] pt-4 text-[18px] text-[#083F46] font-bold">
+          <p>full name</p>
+          <p>gender</p>
+          <p>email</p>
+          <p className="pl-[80px]">phone number</p>
+        </div>
+
+        {data.map((item, index) => (
+          <div className="each-contact-row flex gap-[30px] items-center my-3" key={index}>
+            <img
+              className="pl-8 pr-5"
+              src="./src/assets/images/male.png"
+              alt="user Image"
+            />
+            <p className="max-w-[173px] pr-12">{item.name}</p>
+            <p className="pr-[90px]">{item.gender}</p>
+            <p className="pr-[75px]">{item.mail}</p>
+            <p className="pr-[30px]">{item.phone}</p>
+            <img className="cursor-pointer" src="./src/assets/images/pencil.svg" alt="" />
+            <img className="cursor-pointer" src="./src/assets/images/delete.svg" alt="" />
+          </div>
+        ))}
+
+        
+        
+      </section>
 
       <a
         onClick={() => go_T0_Login()}
